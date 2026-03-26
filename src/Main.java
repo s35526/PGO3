a9360d6997e62b318d6dc9edd05d9478f1bdcc2a
s35526 <s35526@pjwstk.edu.pl>
2026-03-26 16:37:57 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ProduktMenu p1 = new ProduktMenu("123", "Latte", 12, "kawa");
+        ProduktMenu p2 = new ProduktMenu("321", "Espresso", 9, "kawa");
+        ProduktMenu p3 = new ProduktMenu("213", "Serniczek", 18, "deser");
+    }
+}
